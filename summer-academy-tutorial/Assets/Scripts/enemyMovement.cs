db2950940373606c8b85da764e23705aa7837d17
Mike DeAnda <mdeanda@depaul.edu>
2020-07-10 14:22:29 -0500

    Added Unity project to folder.

---
+++ summer-academy-tutorial/Assets/Scripts/enemyMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyMovement : MonoBehaviour
+{
+    //first identify a rigid body that will be moved
+    public Rigidbody rb;
+    public Vector3 movement;
+    public float power;
+    //public/private, obj variable that you are using, variable name
+   
+    //input types:
+     //float 1.555f
+     //int 1
+     //string "series of characters in quotation marks"
+     //bool true/false
+     //GameObjects
+      
+
+    //need to move the rigid body towards the opposite side of the platform
+
+    //private vs public
+    //void = function
+    //name function "Update" (variable or obj)
+
+    private void Update()
+        //{
+    {
+       
+        //AddForce uses a Vecotr3(x,y,z)
+        rb.AddForce(movement * power);
+   
+    }
+
+
+}
---
+++ summer-academy-tutorial/Assets/Scripts/killBox.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class killBox : MonoBehaviour
+{
+    //this script will destroy any game objects that move into it.
+    //check if there is an event that triggers
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("enter trigger");
+
+        //check if object is tagged enemy
+        if (other.CompareTag("enemy"))
+        {
+            Destroy(other.gameObject);
+            Debug.Log("enemy detected");
+            Debug.Log("hi rob");
+        }
+    }
+    //destroy the object
+
+
+}
